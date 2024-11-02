5da2afeb6b00c82bd14c83e53625301e31d65d4f
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2024-11-03 00:49:21 +0100

    change div to img

--- source/js/app.js
+++ source/js/app.js
@@ -124,7 +124,7 @@ function refreshBoard() {
                             `<div class="task bg-gray-700 rounded-md mt-3 border border-gray-600" data-task-id="${task.id}">
                                 <div class="parent w-full bg-gray-800 p-2 rounded flex justify-between border-b border-gray-600 relative">
                                     <div class="flex items-center">
-                                        <div class="w-8 h-8 rounded-full bg-white mr-3"></div>
+                                        <img src="./source/img/photo youcode.jpg" alt="" class="border-2 border-zinc-400 w-8 h-8 rounded-full mr-3">
                                         <p class="mr-3 text-white font-poppins">Mostafa</p>
                                         <span class="px-2 text-sm font-medium text-center text-white rounded-full ${findObject(priorities, task.priority).name == 'P1' ? 'bg-red-500 border-2 border-red-700' : findObject(priorities, task.priority).name  == 'P2' ? 'bg-orange-500 border-2 border-orange-700' : 'bg-green-500 border-2 border-green-700'} mr-3">${findObject(priorities, task.priority).name }</span>
                                     </div>
